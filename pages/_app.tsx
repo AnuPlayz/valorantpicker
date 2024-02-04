0921b097bca5dd7a0bf18e22f14b0d734472c70f
Aniruddh <adubge@gmail.com>
2024-02-04 23:38:38 +0530

    Add viewport meta tag for responsive design

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@/components/ui/theme-provider"
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
         enableSystem
         disableTransitionOnChange
       >
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        </Head>
         <Component {...pageProps} />
       </ThemeProvider>
     </>
