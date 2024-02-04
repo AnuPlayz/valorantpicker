a54a40e687af4e408afd7e44251c4900389ac374
Aniruddh <adubge@gmail.com>
2024-02-04 21:53:47 +0530

    Add ESLint ignoreDuringBuilds option to next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     domains: ['i.pinimg.com'],
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
