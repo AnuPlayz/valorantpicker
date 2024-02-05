e957611f95b903a07713255c490fe0be93cd94c5
Aniruddh <adubge@gmail.com>
2024-02-05 07:38:14 +0530

    Add @vercel/analytics package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@/components/ui/theme-provider"
 import Head from "next/head";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -15,7 +16,8 @@ export default function App({ Component, pageProps }: AppProps) {
         <Head>
           <meta name="viewport" content="width=1024" />
         </Head>
-          <Component {...pageProps} />
+        <Component {...pageProps} />
+        <Analytics />
       </ThemeProvider>
     </>
   );
